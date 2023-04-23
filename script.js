09543d182d36738f8c11884eb31131618e0bfcea
Ashleigh-T <ash0606@me.com>
2023-04-23 18:58:39 -0400

    added functions C-rankings and Next Race Stats

--- script.js
+++ script.js
@@ -1,13 +1,41 @@
 
 function injectDriverRankings(driverNames,driverPoints){
     console.log('fired injectDriverRankings');
-    for (let i = 1; i < 21; i++){
-        let name = document.querySelector('#name'+i);
+    for (let i = 0; i < 20; i++){
+        let name = document.querySelector('#name'+i+1);
             console.log("rank "+ i + ": " + driverNames(i));
             name.innerText = driverNames(i);
-        let points = document.querySelector('#points'+i);
+        let points = document.querySelector('#points'+i+1);
             console.log("points: " + driverPoints(i));
             points.innerText = driverPoints(i);
     }
     console.log('injectDriverRankings done');
+}
+
+// if this works with parent selectors change the above method and HTML for the above method as well 
+function injectConstructorRankings(constructorsRank,constructorsPoints,constructorImage){
+    console.log('fired injectConstructorsRankings');
+    for(let i = 0; i <10; i++){
+        let parent = document.querySelector('#CR'+i+1);
+        let rank = parent.querySelector('rank');
+            rank.innerText = constructorsRank(i);
+        let img = parent.querySelector('image');
+            img.innerHTML = "<img class = 'image' src = '" + constructorImage(i) + '"/>';
+        let points = parent.querySelector('points');
+            points.innerText = constructorsPoints(i);
+    }
+}
+
+function injectNextRaceStats(list,img){
+    console.log('fired injectNextRaceStats');
+    let name = document.querySelector('#NRS1');
+        name.innerText = list(0);
+    let country = document.querySelector('#NRS2');
+        country.innerText = list(1);
+        let img = country.querySelector(".image");
+        img.innerHTML = "<img class = 'image' scr = '" + img +"'/>";
+    let time = document.querySelector('#NRS#');
+        time.innerText = list(2);
+    let round = document.querySelector("#NRS4");
+        round.innerText = list(3);
 }
