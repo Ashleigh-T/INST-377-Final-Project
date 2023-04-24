b9eccb9be841d121d128847def0b409e225310d9
Ashleigh-T <ash0606@me.com>
2023-04-23 20:34:13 -0400

    commented issues i need to work on

--- script.js
+++ script.js
@@ -1,48 +1,51 @@
 
-function injectDriverRankings(driverNames,driverPoints){
+/*
+    current i have the bare bones done for this for the main page 
+    in the un asyc functions i need to work with the json paths to get the data 
+*/
+
+
+function injectDriverRankings(info){
     console.log('fired injectDriverRankings');
     for (let i = 0; i < 20; i++){
         let name = document.querySelector('#name'+i+1);
-            console.log("rank "+ i + ": " + driverNames(i));
-            name.innerText = driverNames(i);
+            name.innerText = // put path to find info here
         let points = document.querySelector('#points'+i+1);
-            console.log("points: " + driverPoints(i));
-            points.innerText = driverPoints(i);
+            points.innerText = // put path to find info here
     }
     console.log('injectDriverRankings done');
 }
 
 // if this works with parent selectors change the above method and HTML for the above method as well 
-function injectConstructorRankings(constructorsRank,constructorsPoints,constructorImage){
+function injectConstructorRankings(info){
     console.log('fired injectConstructorsRankings');
     for(let i = 0; i <10; i++){
         let parent = document.querySelector('#CR'+i+1);
         let rank = parent.querySelector('rank');
-            rank.innerText = constructorsRank(i);
+            rank.innerText = // put path to find info here
         let img = parent.querySelector('image');
-            img.innerHTML = "<img class = 'image' src = '" + constructorImage(i) + '"/>';
+            img.innerHTML = "<img class = 'image' src = '" + // put path to find info here + '"/>';
         let points = parent.querySelector('points');
-            points.innerText = constructorsPoints(i);
+            points.innerText = // put path to find info here
     }
 }
 
-function injectNextRaceStats(list,img){
+function injectNextRaceStats(info){
     console.log('fired injectNextRaceStats');
     let name = document.querySelector('#NRS1');
-        name.innerText = list(0);
+        name.innerText = // put path to find info here 
     let country = document.querySelector('#NRS2');
-        country.innerText = list(1);
-        let img = country.querySelector(".image");
-        img.innerHTML = "<img class = 'image' scr = '" + img +"'/>";
+        country.innerText = // put path to find info here
     let time = document.querySelector('#NRS#');
-        time.innerText = list(2);
+        time.innerText = // put path to find info here 
     let round = document.querySelector("#NRS4");
-        round.innerText = list(3);
+        round.innerText = // put path to find info here 
 }
 
 async function mainEvent(){
 
-    const storedData = localStorage.getItem('storedData');
+// this is all for the main page so far 
+// below are the varibles that need to be global for everything to work 
     const logoImg = new Map([
         ['AlfaRomeo','Photos/AlfaRomeo/alfa romeo logo.jpg'],
         ['AlphaTauri','Photos/AlphaTauri/alpha tauri logo.jpg'],
@@ -55,23 +58,30 @@ async function mainEvent(){
         ['RedBull','Photos/RedBull/redbull logo.jpg'],
         ['Williams','Photos/Williams/Williams logo.jpg']
     ]);
-    let date = newDate();
 
+    const date = newDate();
 
-    const driverRankings = await fetch('http://ergast.com/api/f1/current/driverStandings');
-    const constructorsRanking = await fetch ('http://ergast.com/api/f1/current/constructorStandings');
-    const raceScedule = await fetch('http://ergast.com/api/f1/{{year}}');
-    const lastRace = await fetch('http://ergast.com/api/f1/current/last/results');
+    let results = await fetch('http://ergast.com/api/f1/current/driverStandings');
+        const driverRankings = await results.json();
 
-    const nextRace = function(){
-        // use lastRace + raceScedule to find next race 
-    }
+    results = await fetch ('http://ergast.com/api/f1/current/constructorStandings');
+        const constructorsRanking = await results.json();
 
-    const nextRaceInfo = await fetch('http://ergast.com/api/f1/circuits/{{'+nextRace+'}}');
+    results = await fetch('http://ergast.com/api/f1/current/last/results');
+        const lastRace = await results.json();
 
-    const nextRaceStats = function(){
-        // use raceSchedule + nextRaceInfo to get stats for injection 
-    }
+    const nextRound = lastRace.MRData.RaceTable.Round + 1;
+
+    const nextRaceInfo = await fetch('http://ergast.com/api/f1/{{'+date+'}}/{{'+nextRound+'}}'); // check the date function might have to parse 
+    // have to move the data around for only this because it the pain in the ass it is to get the data i want but everything else should work 
+    // with just the .json results 
+
+    // all inital injections here 
+    injectConstructorRankings(constructorsRanking);
+    injectDriverRankings(driverRankings);
+    injectNextRaceStats(nextRaceInfo);
+
+    // all query selectors here 
     
 }
 
