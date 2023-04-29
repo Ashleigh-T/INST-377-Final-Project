7cc5502b7a551acbbe8343eb7e02a7f365ec5c8a
Ashleigh-T <ash0606@me.com>
2023-04-28 20:02:37 -0400

    injection for all constructor stuff done

--- script.js
+++ script.js
@@ -22,18 +22,42 @@ function injectDriversPoints(driver_rankings){
         //console.log(driver_rankings.MRData.StandingsTable.StandingsLists[0].DriverStandings[i].points);
         //console.log(spot);
     };
-}
+};
 
-function injectConstructorRank(index){
-    for(index ; index < index+5; index++){
+function injectConstructorRank(r_index){
+    for(let index = 0 ; index < 5 ; index++){
+        const box = document.querySelector('#CR'+(index+1));
+        //console.log(box);
+        const spot = box.querySelector('.rank');
+        //console.log(spot);
+        spot.innerHTML = (r_index);
+        r_index++;
+        //console.log(spot);
+    }
+};
+
+function injectConstructorPoints(r_index,constructors_ranking){
+    for(let index = 0; index < 5; index++){
+        const box = document.querySelector('#CR'+(index+1));
+        //console.log(box);
+        const spot = box.querySelector('.points');
+        //console.log(spot)
+        spot.innerHTML = constructors_ranking.MRData.StandingsTable.StandingsLists[0].ConstructorStandings[r_index].points;
+        //console.log(constructors_ranking.MRData.StandingsTable.StandingsLists[0].ConstructorStandings[r_index].points);
+        r_index++;
+    };
+};
+
+function injectConstructorLogos(r_index,constructors_ranking,logos){
+    for(let index = 0; index < 5; index++){
         const box = document.querySelector('#CR'+(index+1));
         console.log(box);
-        const spot = box.querySelector('.rank');
-        console.log(spot);
-        spot.innerHTML = (index + 1);
+        const key = constructors_ranking.MRData.StandingsTable.StandingsLists[0].ConstructorStandings[r_index].Constructor.name;
+        console.log(logos.get(key));
+        box.querySelector('img').src = logos.get(key);
+        r_index++;
     }
 }
-
 
 async function mainEvent(){
 
@@ -69,23 +93,23 @@ team_page.forEach( item => {
  const left_button = document.querySelector("#left_button");
  const right_button = document.querySelector('#right_button');
  const logo_img = new Map([
-        ['AlfaRomeo','Photos/AlfaRomeo/alfa romeo logo.jpg'],
-        ['AlphaTauri','Photos/AlphaTauri/alpha tauri logo.jpg'],
-        ['Alpine','Photos/Alpine/alpine logo.jpg'],
-        ['AstonMartin',"Photos/AstonMartin/Aston-Martin logo.jpg"],
-        ['Ferrari','Photos/Ferrari/Ferrari-Logo.jpg'],
-        ['Hass','Photos/Hass/haas logo.jpg'],
-        ['Mclaren','Photos/Mclaren/mclaren logo.jpg'],
-        ['Mercedes','Photos/Mercedes/mercedes logo.jpg'],
-        ['RedBull','Photos/RedBull/redbull logo.jpg'],
-        ['Williams','Photos/Williams/Williams logo.jpg']
+        ['Alfa Romeo','Final_Project/Photos/AlfaRomeo/alfa romeo logo.jpg'],
+        ['AlphaTauri','Final_Project/Photos/AlphaTauri/alpha tauri logo.jpg'],
+        ['Alpine F1 Team','Final_Project/Photos/Alpine/alpine logo.jpg'],
+        ['Aston Martin',"Final_Project/Photos/AstonMartin/Aston-Martin logo.jpg"],
+        ['Ferrari','Final_Project/Photos/Ferrari/Ferrari-Logo.jpg'],
+        ['Hass F1 Team','Final_Project/Photos/Hass/haas logo.jpg'],
+        ['McLaren','Final_Project/Photos/Mclaren/mclaren logo.jpg'],
+        ['Mercedes','Final_Project/Photos/Mercedes/mercedes logo.jpg'],
+        ['Red Bull','Final_Project/Photos/RedBull/redbull logo.jpg'],
+        ['Williams','Final_Project/Photos/Williams/Williams logo.jpg']
     ]);
 
     
 
  let results = await fetch('http://ergast.com/api/f1/current/driverStandings.json');
     const driver_rankings = await results.json();
-    console.log(driver_rankings);
+    //console.log(driver_rankings);
 
  results = await fetch ('http://ergast.com/api/f1/current/constructorStandings.json');
     const constructors_ranking = await results.json();
@@ -98,8 +122,9 @@ team_page.forEach( item => {
     injectDriverNames(driver_rankings);
     injectDriversPoints(driver_rankings);
 
-    injectConstructorRank(0);
-
+    injectConstructorRank(1);
+    injectConstructorPoints(0,constructors_ranking);
+    injectConstructorLogos(0,constructors_ranking,logo_img);
 }
 
 document.addEventListener("DOMContentLoaded", async () => mainEvent());
