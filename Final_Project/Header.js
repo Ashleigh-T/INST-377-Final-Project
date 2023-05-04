5d6c53b7394fce2641e60e9ee22add82064a67b1
Ashleigh-T <ash0606@me.com>
2023-05-04 12:45:22 -0400

    controls passing varibles to local storage for DPL

---
+++ Final_Project/Header.js
@@ -0,0 +1,21 @@
+
+async function mainEvent(){
+const driver_page = document.querySelectorAll('.driver_button');
+const team_page = document.querySelectorAll('.team_button');
+
+driver_page.forEach( item => {
+    item.addEventListener('click', (event) => {
+        console.log(item.innerHTML);
+        localStorage.setItem('Driver',item.innerHTML)
+    }); 
+});
+
+team_page.forEach( item => {
+    item.addEventListener('click',(event) => {
+        console.log(item.innerHTML);
+        localStorage.setItem('Team',item.innerHTML)
+    });
+});
+};
+
+document.addEventListener("DOMContentLoaded", async () => mainEvent());
